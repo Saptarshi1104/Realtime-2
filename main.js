b1975f09b3cd0fd430eee044114a7aacabf0bf43
Saptarshi Mandal <80155876+Saptarshi1104@users.noreply.github.com>
2021-09-15 20:41:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload(){
+
+}
+
+function setup(){
+canvas = createCanvas(300, 300);
+canvas.center();
+video = createCapture(VIDEO);
+video.hide();
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json', modelLoaded);
+}
+
+function modelLoaded(){
+console.log("model loaded!");
+}
+
+function draw(){
+image(video, 0, 0, 300, 300);
+classifier.classify(video, gotResult);
+}
